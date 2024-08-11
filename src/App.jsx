99f84eec9035714082b947a7904e7a7a18d656ba
jgar514 <j.garvey514@yahoo.com>
2024-08-11 17:35:27 -0400

    fixed light reload problem

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-// src/App.jsx
-import React, { useState, useRef } from 'react';
-
+import React, { useState, useRef, useEffect } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import Model from './Model';
@@ -10,32 +8,28 @@ import LakeClaire from './LakeClaire';
 import LittleMagothy from './LittleMagothy';
 import BroadneckPark from './BroadneckPark';
 import Church from './Church';
-// import Floor from './Floor';
 import Cube from './Cube';
 import Trees from './Trees';
-// import Island from './Island';
 import Broadneck from './Broadneck';
 import Shops from './Shops';
 import DetailPage from './Detail';
 import { IoIosSearch } from "react-icons/io";
 import WeatherCard from './WeatherCard';
-import useResponsiveFov from './useResponsiveFov'; // Import the custom hook
+import useResponsiveFov from './useResponsiveFov';
 import DeepCreek from './DeepCreek';
 import LittleBeach from './LittleBeach';
 import CapeField from './CapeField';
-// import BneckSchool from './BneckSchool';
 import CscElem from './CscElem';
-// import BorderTrees from './BorderTrees';
 import MainBeach from './MainBeach';
 import BoatRamp from './BoatRamp';
-// import BneckPark from './BneckPark';
 import CapeClubhouse from './CapeClubhouse';
 import GoshenFarm from './GoshenFarm';
 import CapeFirehouse from './CapeFirehouse';
+
 const App = () => {
   const [showDetail, setShowDetail] = useState(false);
-  const fov = useResponsiveFov(); // Get the responsive FOV
-
+  const [spotlightsInitialized, setSpotlightsInitialized] = useState(false);
+  const fov = useResponsiveFov();
 
   const lampRef = useRef();
   const targetRef = useRef();
@@ -47,6 +41,49 @@ const App = () => {
   const seventhTargetRef = useRef();
   const eigthTargetRef = useRef();
 
+  const spotLight1 = useRef();
+  const spotLight2 = useRef();
+  const spotLight3 = useRef();
+  const spotLight4 = useRef();
+  const spotLight5 = useRef();
+  const spotLight6 = useRef();
+  const spotLight7 = useRef();
+  const spotLight8 = useRef();
+
+  useEffect(() => {
+    // Use a delay to ensure targets are assigned after everything has been initialized
+    const timer = setTimeout(() => {
+      if (spotLight1.current && targetRef.current) {
+        spotLight1.current.target = targetRef.current;
+      }
+      if (spotLight2.current && secondTargetRef.current) {
+        spotLight2.current.target = secondTargetRef.current;
+      }
+      if (spotLight3.current && thirdTargetRef.current) {
+        spotLight3.current.target = thirdTargetRef.current;
+      }
+      if (spotLight4.current && fourthTargetRef.current) {
+        spotLight4.current.target = fourthTargetRef.current;
+      }
+      if (spotLight5.current && fifthTargetRef.current) {
+        spotLight5.current.target = fifthTargetRef.current;
+      }
+      if (spotLight6.current && sixthTargetRef.current) {
+        spotLight6.current.target = sixthTargetRef.current;
+      }
+      if (spotLight7.current && seventhTargetRef.current) {
+        spotLight7.current.target = seventhTargetRef.current;
+      }
+      if (spotLight8.current && eigthTargetRef.current) {
+        spotLight8.current.target = eigthTargetRef.current;
+      }
+      // Set state to trigger a re-render if necessary
+      setSpotlightsInitialized(true);
+    }, 500); // Adjust delay time as needed
+
+    return () => clearTimeout(timer); // Cleanup on unmount
+  }, []);
+
   const handleCubeClick = () => {
     setShowDetail(true);
   };
@@ -54,7 +91,6 @@ const App = () => {
   const handleCloseDetail = () => {
     setShowDetail(false);
   };
-
 
   return (
     <div className="w-full h-screen relative flex flex-col">
@@ -81,149 +117,54 @@ const App = () => {
             <cylinderGeometry args={[0.1, 0.1, 0.5, 32]} />
             <meshStandardMaterial color="white" />
           </mesh>
-          {/* 
-          <spotLight
-            position={[0, .3, 0]}
-            angle={Math.PI / 30}
-            penumbra={0.2}
-            intensity={2}
-            distance={50}
-            decay={2}
-            castShadow
-            target={targetRef.current}
-          /> */}
-          <spotLight
-            position={[-.7, 2, 1]}
-            angle={Math.PI / 12}
-            penumbra={0.2}
-            intensity={8}
-            distance={50}
-            decay={2}
-            castShadow
-            target={targetRef.current}
-          />
+
+          <spotLight ref={spotLight1} position={[-.7, 2, 1]} angle={Math.PI / 12} penumbra={0.2} intensity={8} distance={50} decay={2} castShadow />
           <mesh ref={targetRef} position={[-.7, 0, .9]}>
             <boxGeometry args={[.9, 0.08, .9]} />
             <meshStandardMaterial color="brown" transparent opacity={0.8} />
           </mesh>
-          {/* <ambientLight intensity={1.2} /> */}
-          {/* <directionalLight position={[2, 1.8, .5]} intensity={1.2} /> */}
-          {/* <rectAreaLight
-            width={.5}
-            height={.5}
-            color={"#EBFE9B"}
-            intensity={45}
-            position={[-.5, .6, 1.5]}
 
-            penumbra={1}
-            castShadow
-          /> */}
+          <spotLight ref={spotLight2} position={[1, 1.5, 1]} angle={Math.PI / 16} penumbra={0.2} intensity={8} distance={50} decay={2} castShadow />
           <mesh ref={secondTargetRef} position={[1.0, -.008, 1]}>
             <boxGeometry args={[.55, 0.06, .55]} />
             <meshStandardMaterial color="green" transparent opacity={1} />
           </mesh>
-          <spotLight
-            position={[1, 1.5, 1]}
-            angle={Math.PI / 16}
-            penumbra={0.2}
-            intensity={8}
-            distance={50}
-            decay={2}
-            castShadow
-            target={secondTargetRef.current}
-          />
-          {/* Third target mesh and spotlight */}
 
-          <spotLight
-            position={[.35, 1, 1]}
-            angle={Math.PI / 9}
-            penumbra={0.2}
-            intensity={1}
-            distance={50}
-            decay={2}
-            castShadow
-            target={thirdTargetRef.current}
-          />
+          <spotLight ref={spotLight3} position={[.35, 1, 1]} angle={Math.PI / 9} penumbra={0.2} intensity={1} distance={50} decay={2} castShadow />
           <mesh ref={thirdTargetRef} position={[.35, 0, 1]}>
             <boxGeometry args={[.6, 0.08, .6]} />
             <meshStandardMaterial color="#4682B4" transparent opacity={1} />
           </mesh>
 
+          <spotLight ref={spotLight4} position={[-.4, 1, -1]} angle={Math.PI / 8} penumbra={0.2} intensity={.8} distance={50} decay={2} castShadow />
           <mesh ref={fourthTargetRef} position={[-.4, -.055, -1]}>
             <boxGeometry args={[.6, 0.08, .6]} />
             <meshStandardMaterial color="#add8e6" transparent opacity={0.9} />
           </mesh>
-          <spotLight
-            position={[-.4, 1, -1]}
-            angle={Math.PI / 8}
-            penumbra={0.2}
-            intensity={.8}
-            distance={50}
-            decay={2}
-            castShadow
-            target={fourthTargetRef.current}
-          />
 
+          <spotLight ref={spotLight5} position={[1.7, 1, -.35]} angle={Math.PI / 6} penumbra={0.2} intensity={1.5} distance={50} decay={2} castShadow />
           <mesh ref={fifthTargetRef} position={[1.7, -.055, -.35]}>
             <boxGeometry args={[.8, 0.08, .8]} />
             <meshStandardMaterial color="#add8e6" transparent opacity={0.8} />
           </mesh>
-          <spotLight
-            position={[1.7, 1, -.35]}
-            angle={Math.PI / 6}
-            penumbra={0.2}
-            intensity={1.5}
-            distance={50}
-            decay={2}
-            castShadow
-            target={fifthTargetRef.current}
-          />
-          {/* Sixth target mesh and spotlight with medium yellow color */}
+
+          <spotLight ref={spotLight6} position={[-.3, 1, 2.2]} angle={Math.PI / 16} penumbra={0.2} intensity={4} distance={50} decay={2} castShadow />
           <mesh ref={sixthTargetRef} position={[-.25, .04, 2.1]} rotation={[Math.PI / 1, -.2, .1]}>
             <boxGeometry args={[.3, 0.08, .45]} />
             <meshStandardMaterial color="#ffd700" transparent opacity={0.8} />
           </mesh>
 
-          <spotLight
-            position={[-.3, 1, 2.2]}
-            angle={Math.PI / 16}
-            penumbra={0.2}
-            intensity={4}
-            distance={50}
-            decay={2}
-            castShadow
-            target={sixthTargetRef.current}
-          />
-
-          {/* Seventh target mesh and spotlight with dark brown color */}
+          <spotLight ref={spotLight7} position={[.37, 1, 1.3]} angle={Math.PI / 20} penumbra={0.2} intensity={3} distance={50} decay={2} castShadow />
           <mesh ref={seventhTargetRef} position={[.37, 0.06, 1.3]}>
             <boxGeometry args={[.16, 0.010, .20]} />
             <meshStandardMaterial color="#654321" transparent opacity={1} />
           </mesh>
-          <spotLight
-            position={[.37, 1, 1.3]}
-            angle={Math.PI / 20}
-            penumbra={0.2}
-            intensity={3}
-            distance={50}
-            decay={2}
-            castShadow
-            target={seventhTargetRef.current}
-          />
+
+          <spotLight ref={spotLight8} position={[-1.7, 1, -.45]} angle={Math.PI / 15} penumbra={0.2} intensity={2} distance={50} decay={2} castShadow />
           <mesh ref={eigthTargetRef} position={[-1.7, -.04, -.45]}>
             <boxGeometry args={[.3, 0.08, .3]} />
             <meshStandardMaterial color="#add8e6" transparent opacity={0.9} />
           </mesh>
-          <spotLight
-            position={[-1.7, 1, -.45]}
-            angle={Math.PI / 15}
-            penumbra={0.2}
-            intensity={2}
-            distance={50}
-            decay={2}
-            castShadow
-            target={eigthTargetRef.current}
-          />
 
           <Bay position={[0, 0, 0]} />
           <BoatRamp position={[0, 0, 0]} />
@@ -243,9 +184,6 @@ const App = () => {
           <Streets position={[0, 0.0, 0]} />
           <Trees position={[0, 0.0, 0]} />
           <Church position={[0, .03, 0]} />
-          {/* <Cube position={[-1, .1, .5]} onClick={handleCubeClick} /> */}
-          {/* <Island position={[0, -.1, 0]} /> */}
-          {/* <Floor position={[0, -.03, 0]} /> */}
           <OrbitControls target={[0, 0, 0]} />
         </Canvas>
         {showDetail && <DetailPage onClose={handleCloseDetail} />}
